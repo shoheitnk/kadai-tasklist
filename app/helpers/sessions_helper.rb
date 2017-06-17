7860eb3eb00bcd9c5f78247f009325c2c155c7cc
Shohey Tanaka <shohei-tanaka@dac.co.jp>
2017-06-17 16:46:37 +0000

    0146

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,9 +1,9 @@
 module SessionsHelper
   def current_user
-    @current_uesr ||= User.find_by(id: session[:user_id])
+    @current_user ||= User.find_by(id: session[:user_id])
   end
   
   def logged_in?
-    !!curren_user
+    !!current_user
   end
 end
