14e245ff226979945209d16e4368c243a276865d
Shohey Tanaka <shohei-tanaka@dac.co.jp>
2017-06-17 15:02:18 +0000

    top page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
+  get 'toppages/index'
+
+  root to: 'toppages#index'
+  get 'signup', to: 'users#show'
+  resources :users, only: [:index, :show, :new, :create]
+
+  get 'users/index'
+
+  get 'users/show'
+
+  get 'users/new'
+
+  get 'users/create'
+
+
   root to: 'tasks#index'
-  
   resources :tasks
 end
